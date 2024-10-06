3dd77e9c83a6ac8cf1e39361fd8dacce58409084
evandeters <evanjd711@gmail.com>
2024-10-06 01:59:03 -0700

    test vm objects

--- vsphere_test.go
+++ vsphere_test.go
@@ -43,14 +43,6 @@ func TestGetVMsInResourcePool(t *testing.T) {
         if vms == nil {
             continue
         }
-        for _, vm := range vmList {
-            vmObj := object.NewVirtualMachine(vSphereClient.client, vm.Reference())
-            vmName, err := vmObj.ObjectName(vSphereClient.ctx)
-            if err != nil {
-                t.Error(err)
-            }
-            log.Println(vmName)
-        }
         vms = append(vms, vmList...)
     }
 
@@ -58,34 +50,37 @@ func TestGetVMsInResourcePool(t *testing.T) {
 }
 
 func TestVMObjects(t *testing.T) {
-    rp, err := GetResourcePool(mainConfig.VCenterConfig.PresetTemplateResourcePool)
+    childRPs, err := GetChildResourcePools(mainConfig.VCenterConfig.PresetTemplateResourcePool)
     if err != nil {
         t.Error(err)
     }
 
-
     vmList := []vm.VM{}
-    vms, err := GetVMsInResourcePool(rp.Reference())
-    for _, v := range vms {
-        vmObj := object.NewVirtualMachine(vSphereClient.client, v.Reference())
-        vmName, err := vmObj.ObjectName(vSphereClient.ctx)
+    for _, rp := range childRPs {
+        vms, err := GetVMsInResourcePool(rp.Reference())
         if err != nil {
             t.Error(err)
         }
-        log.Println(vmName)
-        newVM := vm.VM{
-            Name: vmName,
-            Ref: v.Reference(),
-            Client: vSphereClient.client,
-            Ctx: &vSphereClient.ctx,
-            IsRouter: false,
-            IsHidden: false,
-            GuestOS: "Test",
-        }
+        for _, v := range vms {
+            vmObj := object.NewVirtualMachine(vSphereClient.client, v.Reference())
+            vmName, err := vmObj.ObjectName(vSphereClient.ctx)
+            if err != nil {
+                t.Error(err)
+            }
+            newVM := vm.VM{
+                Name: vmName,
+                Ref: v.Reference(),
+                Client: vSphereClient.client,
+                Ctx: &vSphereClient.ctx,
+                IsRouter: false,
+                IsHidden: false,
+                GuestOS: "Test",
+            }
 
-        resString := fmt.Sprintf("VM: %s\nUsername: %s\nPassword: %s\nIs Router: %v\nIs Hidden: %v\nGuest OS: %s\n", newVM.Name, newVM.Username, newVM.Password, newVM.IsRouter, newVM.IsHidden, newVM.GuestOS)
-        assert.Equal(t, resString, newVM.String())
-        vmList = append(vmList, newVM)
+            resString := fmt.Sprintf("VM: %s\nUsername: %s\nPassword: %s\nIs Router: %v\nIs Hidden: %v\nGuest OS: %s\n", newVM.Name, newVM.Username, newVM.Password, newVM.IsRouter, newVM.IsHidden, newVM.GuestOS)
+            assert.Equal(t, resString, newVM.String())
+            vmList = append(vmList, newVM)
+        }
     }
 
     assert.NotEmpty(t, vmList)
