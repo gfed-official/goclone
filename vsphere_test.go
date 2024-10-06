e095e839c3e8542532a6117fb30f60e678dbb6bf
evandeters <evanjd711@gmail.com>
2024-10-06 13:59:44 -0700

    testing more test cases

--- vsphere_test.go
+++ vsphere_test.go
@@ -33,3 +33,71 @@ func TestHealth(t *testing.T) {
 		Status(http.StatusOK).
 		JSON().Object().HasValue("status", "ok")
 }
+
+func TestViewPresetTemplates(t *testing.T) {
+	router := gin.Default()
+	private := router.Group("/api/v1")
+	addPrivateRoutes(private)
+
+	e := httpexpect.WithConfig(httpexpect.Config{
+		Client: &http.Client{
+			Transport: httpexpect.NewBinder(router),
+			Jar:       httpexpect.NewCookieJar(),
+		},
+		Reporter: httpexpect.NewAssertReporter(t),
+		Printers: []httpexpect.Printer{
+			httpexpect.NewDebugPrinter(t, true),
+		},
+	})
+
+	e.GET("/api/v1/view/templates/preset").
+		Expect().
+		Status(http.StatusOK).
+		JSON().Object().ContainsKey("templates")
+}
+
+func TestViewCustomTemplates(t *testing.T) {
+	router := gin.Default()
+	private := router.Group("/api/v1")
+	addPrivateRoutes(private)
+
+	e := httpexpect.WithConfig(httpexpect.Config{
+		Client: &http.Client{
+			Transport: httpexpect.NewBinder(router),
+			Jar:       httpexpect.NewCookieJar(),
+		},
+		Reporter: httpexpect.NewAssertReporter(t),
+		Printers: []httpexpect.Printer{
+			httpexpect.NewDebugPrinter(t, true),
+		},
+	})
+
+	e.GET("/api/v1/view/templates/custom").
+		Expect().
+		Status(http.StatusOK).
+		JSON().Object().ContainsKey("templates")
+}
+
+func TestTemplateClone(t *testing.T) {
+	router := gin.Default()
+	private := router.Group("/api/v1")
+	addPrivateRoutes(private)
+
+	e := httpexpect.WithConfig(httpexpect.Config{
+		Client: &http.Client{
+			Transport: httpexpect.NewBinder(router),
+			Jar:       httpexpect.NewCookieJar(),
+		},
+		Reporter: httpexpect.NewAssertReporter(t),
+		Printers: []httpexpect.Printer{
+			httpexpect.NewDebugPrinter(t, true),
+		},
+	})
+
+	e.POST("/api/v1/pod/clone/template").
+		WithJSON(map[string]interface{}{
+			"template": "template-1",
+		}).WithCookie("kamino", "MTcyODA4NzU5OXxEWDhFQVFMX2dBQUJFQUVRQUFBal80QUFBUVp6ZEhKcGJtY01CQUFDYVdRR2MzUnlhVzVuREFrQUIyVmtaWFJsY25NPXysdtKrDBsuXNGif07Ye1yLOgx7U3dePLw7-jOXCpedQg==").
+		Expect().
+		Status(http.StatusOK)
+}
